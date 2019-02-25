2d460f9e154907ec11dc5a696d8dd5251b90e971
jojo-la-bricole <42742979+jojo-la-bricole@users.noreply.github.com>
2019-02-25 16:31:54 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -15,3 +15,4 @@ class Users(models.Model):
     name2 = fields.Char(string='Name')
     name3 = fields.Char(string='Name')
     name4 = fields.Char(string='Name')
+    name5 = fields.Char(string='Name')
