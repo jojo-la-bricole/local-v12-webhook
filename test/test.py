855cec497e961415ca8e22aee16af15d4e50b716
Fabien Meghazi <agr@amigrave.com>
2019-02-25 16:06:48 +0100

    BOOM

---
+++ test/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import api, models, modules, _
+
+_logger = logging.getLogger(__name__)
+
+
+class Users(models.Model):
+    _name = 'res.users'
+    _inherit = ['res.users']
+
+    name2 = fields.Char(string='Name')
+    name3 = fields.Char(string='Name')
